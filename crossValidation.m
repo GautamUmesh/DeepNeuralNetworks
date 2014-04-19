2c2b8b4d2df533bbb751c1679bae62d92f3016db
Gautam Umesh Sargur <gautham.umesh@gmail.com>
2014-04-19 14:15:38 -0500

    Add cross-validation step

---
+++ crossValidation.m
@@ -0,0 +1,31 @@
+trainData = loadMNISTImages('mnist/train-images-idx3-ubyte');
+trainLabels = loadMNISTLabels('mnist/train-labels-idx1-ubyte');
+
+trainLabels(trainLabels == 0) = 10;
+acc = zeros(10, 1);
+
+for i=1:10
+    inputTrain = zeros(size(trainData, 1), size(trainData, 2)-6000);
+    inputTest = zeros(size(trainData, 1), 6000);
+    labelTrain = zeros(size(trainLabels, 1)-6000, 1);
+    labelTest = zeros(6000, 1);
+    k = 1;
+    l = 1;
+    for j=1:size(trainData, 2)
+        if (j < (i-1)*6000+1 || j > i*6000)
+            inputTrain(:,k) = trainData(:,j);
+            labelTrain(k,:) = trainLabels(j,:);
+            k = k+1;
+        else
+            inputTest(:,l) = trainData(:,j);
+            labelTest(l,:) = trainLabels(j,:);
+            l = l+1;
+        end
+    end
+    size(inputTrain)
+    size(inputTest)
+    size(labelTrain)
+    size(labelTest)
+    pause;
+    acc(i,1) = stackedAE(inputTrain, labelTrain, inputTest, labelTest);
+end
--- stackedAE.m
+++ stackedAE.m
@@ -1,21 +1,22 @@
+function [acc] = stackedAE(trainData, trainLabels, testData, testLabels)
 %% CS294A/CS294W Stacked Autoencoder Exercise
 
 %  Instructions
 %  ------------
-% 
+%
 %  This file contains code that helps you get started on the
 %  sstacked autoencoder exercise. You will need to complete code in
 %  stackedAECost.m
-%  You will also need to have implemented sparseAutoencoderCost.m and 
+%  You will also need to have implemented sparseAutoencoderCost.m and
 %  softmaxCost.m from previous exercises. You will need the initializeParameters.m
 %  loadMNISTImages.m, and loadMNISTLabels.m files from previous exercises.
-%  
+%
 %  For the purpose of completing the assignment, you do not need to
-%  change the code in this file. 
+%  change the code in this file.
 %
 %%======================================================================
 %% STEP 0: Here we provide the relevant parameters values that will
-%  allow your sparse autoencoder to get good filters; you do not need to 
+%  allow your sparse autoencoder to get good filters; you do not need to
 %  change the parameters below.
 
 inputSize = 28 * 28;
@@ -23,10 +24,10 @@ numClasses = 10;
 hiddenSizeL1 = 500;    % Layer 1 Hidden Size
 hiddenSizeL2 = 200;    % Layer 2 Hidden Size
 sparsityParam = 0.1;   % desired average activation of the hidden units.
-                       % (This was denoted by the Greek alphabet rho, which looks like a lower-case "p",
-		               %  in the lecture notes). 
-lambda = 3e-3;         % weight decay parameter       
-beta = 3;              % weight of sparsity penalty term       
+% (This was denoted by the Greek alphabet rho, which looks like a lower-case "p",
+%  in the lecture notes).
+lambda = 3e-3;         % weight decay parameter
+beta = 3;              % weight of sparsity penalty term
 
 %%======================================================================
 %% STEP 1: Load data from the MNIST database
@@ -34,10 +35,10 @@ beta = 3;              % weight of sparsity penalty term
 %  This loads our training data from the MNIST database files.
 
 % Load MNIST database files
-trainData = loadMNISTImages('mnist/train-images-idx3-ubyte');
-trainLabels = loadMNISTLabels('mnist/train-labels-idx1-ubyte');
-
-trainLabels(trainLabels == 0) = 10; % Remap 0 to 10 since our labels need to start from 1
+% trainData = loadMNISTImages('mnist/train-images-idx3-ubyte');
+% trainLabels = loadMNISTLabels('mnist/train-labels-idx1-ubyte');
+%
+% trainLabels(trainLabels == 0) = 10; % Remap 0 to 10 since our labels need to start from 1
 
 %%======================================================================
 %% STEP 2: Train the first sparse autoencoder
@@ -57,19 +58,19 @@ sae1Theta = initializeParameters(hiddenSizeL1, inputSize);
 
 addpath minFunc/
 options.Method = 'lbfgs'; % Here, we use L-BFGS to optimize our cost
-                          % function. Generally, for minFunc to work, you
-                          % need a function pointer with two outputs: the
-                          % function value and the gradient. In our problem,
-                          % sparseAutoencoderCost.m satisfies this.
-options.maxIter = 100;	  % Maximum number of iterations of L-BFGS to run 
+% function. Generally, for minFunc to work, you
+% need a function pointer with two outputs: the
+% function value and the gradient. In our problem,
+% sparseAutoencoderCost.m satisfies this.
+options.maxIter = 100;	  % Maximum number of iterations of L-BFGS to run
 options.display = 'on';
 
 
 [sae1OptTheta, cost] = minFunc( @(p) sparseAutoencoderCost(p, ...
-                                   inputSize, hiddenSizeL1, ...
-                                   lambda, sparsityParam, ...
-                                   beta, trainData), ...
-                              sae1Theta, options);
+    inputSize, hiddenSizeL1, ...
+    lambda, sparsityParam, ...
+    beta, trainData), ...
+    sae1Theta, options);
 % -------------------------------------------------------------------------
 
 
@@ -82,7 +83,7 @@ options.display = 'on';
 %  to change anything here.
 
 [sae1Features] = feedForwardAutoencoder(sae1OptTheta, hiddenSizeL1, ...
-                                        inputSize, trainData);
+    inputSize, trainData);
 
 %  Randomly initialize the parameters
 sae2Theta = initializeParameters(hiddenSizeL2, hiddenSizeL1);
@@ -95,10 +96,10 @@ sae2Theta = initializeParameters(hiddenSizeL2, hiddenSizeL1);
 %                You should store the optimal parameters in sae2OptTheta
 
 [sae2OptTheta, cost] = minFunc( @(p) sparseAutoencoderCost(p, ...
-                                   hiddenSizeL1, hiddenSizeL2, ...
-                                   lambda, sparsityParam, ...
-                                   beta, sae1Features), ...
-                              sae2Theta, options);
+    hiddenSizeL1, hiddenSizeL2, ...
+    lambda, sparsityParam, ...
+    beta, sae1Features), ...
+    sae2Theta, options);
 % -------------------------------------------------------------------------
 
 
@@ -109,7 +110,7 @@ sae2Theta = initializeParameters(hiddenSizeL2, hiddenSizeL1);
 %  to change anything here.
 
 [sae2Features] = feedForwardAutoencoder(sae2OptTheta, hiddenSizeL2, ...
-                                        hiddenSizeL1, sae1Features);
+    hiddenSizeL1, sae1Features);
 
 %  Randomly initialize the parameters
 saeSoftmaxTheta = 0.005 * randn(hiddenSizeL2 * numClasses, 1);
@@ -120,7 +121,7 @@ saeSoftmaxTheta = 0.005 * randn(hiddenSizeL2 * numClasses, 1);
 %                input of dimension "hiddenSizeL2" corresponding to the
 %                hidden layer size of the 2nd layer.
 %
-%                You should store the optimal parameters in saeSoftmaxOptTheta 
+%                You should store the optimal parameters in saeSoftmaxOptTheta
 %
 %  NOTE: If you used softmaxTrain to complete this part of the exercise,
 %        set saeSoftmaxOptTheta = softmaxModel.optTheta(:);
@@ -128,7 +129,7 @@ saeSoftmaxTheta = 0.005 * randn(hiddenSizeL2 * numClasses, 1);
 lambda = 1e-4;
 options.maxIter = 100;
 softmaxModel = softmaxTrain(hiddenSizeL2, numClasses, lambda, ...
-                            sae2Features, trainLabels, options);
+    sae2Features, trainLabels, options);
 % -------------------------------------------------------------------------
 
 saeSoftmaxOptTheta = softmaxModel.optTheta(:);
@@ -142,10 +143,10 @@ saeSoftmaxOptTheta = softmaxModel.optTheta(:);
 % Initialize the stack using the parameters learned
 stack = cell(2,1);
 stack{1}.w = reshape(sae1OptTheta(1:hiddenSizeL1*inputSize), ...
-                     hiddenSizeL1, inputSize);
+    hiddenSizeL1, inputSize);
 stack{1}.b = sae1OptTheta(2*hiddenSizeL1*inputSize+1:2*hiddenSizeL1*inputSize+hiddenSizeL1);
 stack{2}.w = reshape(sae2OptTheta(1:hiddenSizeL2*hiddenSizeL1), ...
-                     hiddenSizeL2, hiddenSizeL1);
+    hiddenSizeL2, hiddenSizeL1);
 stack{2}.b = sae2OptTheta(2*hiddenSizeL2*hiddenSizeL1+1:2*hiddenSizeL2*hiddenSizeL1+hiddenSizeL2);
 
 % Initialize the parameters for the deep model
@@ -154,7 +155,7 @@ stackedAETheta = [ saeSoftmaxOptTheta ; stackparams ];
 
 %% ---------------------- YOUR CODE HERE  ---------------------------------
 %  Instructions: Train the deep network, hidden size here refers to the '
-%                dimension of the input to the classifier, which corresponds 
+%                dimension of the input to the classifier, which corresponds
 %                to "hiddenSizeL2".
 %
 %
@@ -163,32 +164,32 @@ stackedAETheta = [ saeSoftmaxOptTheta ; stackparams ];
 % -------------------------------------------------------------------------
 
 [stackedAEOptTheta, cost] = minFunc( @(p) stackedAECost(p, ...
-                                   inputSize, hiddenSizeL2, ...
-                                   numClasses, netconfig, ...
-                                   lambda, trainData, trainLabels), ...
-                              stackedAETheta, options);
+    inputSize, hiddenSizeL2, ...
+    numClasses, netconfig, ...
+    lambda, trainData, trainLabels), ...
+    stackedAETheta, options);
 
 %%======================================================================
-%% STEP 6: Test 
+%% STEP 6: Test
 %  Instructions: You will need to complete the code in stackedAEPredict.m
 %                before running this part of the code
 %
 
 % Get labelled test images
 % Note that we apply the same kind of preprocessing as the training set
-testData = loadMNISTImages('mnist/t10k-images-idx3-ubyte');
-testLabels = loadMNISTLabels('mnist/t10k-labels-idx1-ubyte');
-
-testLabels(testLabels == 0) = 10; % Remap 0 to 10
+% testData = loadMNISTImages('mnist/t10k-images-idx3-ubyte');
+% testLabels = loadMNISTLabels('mnist/t10k-labels-idx1-ubyte');
+%
+% testLabels(testLabels == 0) = 10; % Remap 0 to 10
 
 [pred] = stackedAEPredict(stackedAETheta, inputSize, hiddenSizeL2, ...
-                          numClasses, netconfig, testData);
+    numClasses, netconfig, testData);
 
 acc = mean(testLabels(:) == pred(:));
 fprintf('Before Finetuning Test Accuracy: %0.3f%%\n', acc * 100);
 
 [pred] = stackedAEPredict(stackedAEOptTheta, inputSize, hiddenSizeL2, ...
-                          numClasses, netconfig, testData);
+    numClasses, netconfig, testData);
 
 acc = mean(testLabels(:) == pred(:));
 fprintf('After Finetuning Test Accuracy: %0.3f%%\n', acc * 100);
@@ -199,7 +200,8 @@ fprintf('After Finetuning Test Accuracy: %0.3f%%\n', acc * 100);
 % Before Finetuning Test Accuracy: 87.7%
 % After Finetuning Test Accuracy:  97.6%
 %
-% If your values are too low (accuracy less than 95%), you should check 
-% your code for errors, and make sure you are training on the 
-% entire data set of 60000 28x28 training images 
+% If your values are too low (accuracy less than 95%), you should check
+% your code for errors, and make sure you are training on the
+% entire data set of 60000 28x28 training images
 % (unless you modified the loading code, this should be the case)
+end
